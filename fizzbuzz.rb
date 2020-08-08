342f3ba5aa21e65aff0256d89dc08ccf4a73e239
Philip Sailer <philsailer@gmail.com>
2020-08-08 02:02:18 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(input_num)
   if (input_num % 3 == 0) && (input_num % 5 == 0)
-    "Fizz"
-  elsif input_num % 3 == 0
-    "Buzz"
-  elsif input_num % 5 == 0
     "FizzBuzz"
+  elsif input_num % 3 == 0
+    "Fizz"
+  elsif input_num % 5 == 0
+    "Buzz"
   else
     nil
   end
