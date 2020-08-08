51c13bf75c5218bbad9be694d4528b0d61454de9
Philip Sailer <philsailer@gmail.com>
2020-08-08 01:59:28 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
+def fizzbuzz(input_num)
+  if input_num % 3 == 0
+    "Fizz"
+  elsif input_num % 5 == 0
+    "Buzz"
+  elsif (input_num % 3 == 0) && (input_num % 5 == 0)
+    "Fizzbuzz"
+  else
+    nil
 end
